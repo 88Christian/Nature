dfa12a30b3c86fdb9486a65e15267763839f9b15
Ibana Christian (CMLPB IT FI 1 / External) <Ibana.external@infineon.com>
2021-06-05 10:00:41 +0800

    responsive navbar

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ $(window).scroll(function() {
         $(".menu-main").removeClass("menu-main-onscroll");
     }
 });
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 600) {
+        $(".check-box").addClass("ham-menu-main-onscroll");
+    } else {
+        $(".check-box").removeClass("ham-menu-main-onscroll");
+    }
+});
